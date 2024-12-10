ead6157ab1909d2aa6accef6708d75cb641b437e
seenu548 <158687111+seenu548@users.noreply.github.com>
2024-12-10 19:02:56 +0530

    Delete project-bolt-bolt-nativescript-js-enq42pir directory

--- project-bolt-bolt-nativescript-js-enq42pir/project/app/shared/todo.model.js
+++
@@ -1,7 +0,0 @@
-export class TodoItem {
-    constructor(title, completed = false) {
-        this.title = title;
-        this.completed = completed;
-        this.id = Date.now().toString();
-    }
-}
